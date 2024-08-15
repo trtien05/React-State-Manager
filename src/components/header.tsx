19a002104cfce101f6688262d4eb29ac49a59ba9
trtien05 <trantien100700@gmail.com>
2024-08-15 17:51:31 +0700

    add dark mode

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,42 @@
+import { Form } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import { useAppDispatch, useAppSelector } from '../redux/hooks';
+import { changeMode } from '../redux/app/appSlide';
+import { useEffect } from 'react';
 
 const Header = () => {
+  const mode = useAppSelector(state => state.app.mode)
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const body = document.querySelector('body');
+    body?.setAttribute('data-bs-theme', mode)
+  }, [mode])
   return (
-    <Navbar className="bg-body-tertiary">
+    <Navbar className="bg-body-tertiary" data-bs-theme={mode}>
       <Container>
         <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            Signed in as: <a href="#login">Mark Otto</a>
-          </Navbar.Text>
+          <Form>
+            <Form.Check
+              defaultChecked={mode === 'light' ? false : true}
+              onChange={(e) => {
+                dispatch(changeMode(e.target.checked === true ? 'dark' : 'light'))
+              }}
+              type="switch"
+              id="custom-switch"
+              label={mode === 'light' ?
+                <Navbar.Text>
+                  Light mode
+                </Navbar.Text> :
+                <Navbar.Text>
+                  Dark mode
+                </Navbar.Text>}
+            />
+          </Form>
+
         </Navbar.Collapse>
       </Container>
     </Navbar>
--- src/main.tsx
+++ src/main.tsx
@@ -2,27 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
-import { store } from './redux/store.ts'
+import { persistor, store } from './redux/store.ts'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+      </PersistGate>
+
     </Provider>
   </React.StrictMode>,
 )
